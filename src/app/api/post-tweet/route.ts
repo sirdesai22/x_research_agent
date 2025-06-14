4569bb2ffc149cfdff5c19369417216be70eda8b
sirdesai22 <sirdesai.work@gmail.com>
2025-06-14 20:28:04 +0530

    corrected Xposting code

--- src/app/api/post-tweet/route.ts
+++ src/app/api/post-tweet/route.ts
@@ -13,36 +13,9 @@ export async function POST(request: Request) {
     const { tw } = await request.json();
     const tweet = await client.v2.tweet(tw);
     console.log(tweet);
-    return NextResponse.json({ tweet: tweet });
+    return NextResponse.json({ tweet: tweet.data });
   } catch (error) {
     console.error("Error posting tweet:", error);
     return NextResponse.json({ message: "Error" }, { status: 500 });
   }
 }
-
-
-
-
-// const credentials = {
-//   consumerKey: String(process.env.TWITTER_API_KEY!),
-//   consumerSecret: String(process.env.TWITTER_API_SECRET!),
-//   accessToken: String(process.env.TWITTER_ACCESS_TOKEN!),
-//   accessTokenSecret: String(process.env.TWITTER_ACCESS_TOKEN_SECRET!),
-//   bearerToken: String(process.env.TWITTER_BEARER_TOKEN!),
-// }
-
-// const client = new Client();
-// await client.login(credentials);
-
-// export async function POST(request: Request) {
-//   try{
-//   const { message } = await request.json();
-
-//   const tweet = await client.tweets.create({text: message});
-//   console.log(tweet);
-//   return NextResponse.json({tweet: tweet});
-//   }catch(error){
-//     console.log(error)
-//     return NextResponse.json({message: "Error"}, {status: 500})
-//   }
-// }
